58f0b260e5c614776dc9b63e1b9e31cfc585b821
Philip Indra Prayitno <philip.prayitno@binus.edu>
2021-04-14 17:20:29 +0700

    feat: scroll to top button

--- header.js
+++ header.js
@@ -12,3 +12,4 @@ document.addEventListener('DOMContentLoaded', function() {
 //https://www.flaticon.com/free-icon/protection-shield-with-a-check-mark_315#
 //https://www.freepik.com/free-icon/badge_933771.htm#query=dollar&position=12
 //https://www.flaticon.com/free-icon/fast-delivery_98999
+//https://www.freepik.com/redirect/aHR0cDovL3d3dy5mbGF0aWNvbi5jb20vZnJlZS1pY29uL3VwLWFycm93XzEzMDg2NA
