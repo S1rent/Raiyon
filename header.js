36b7d0a9dd32a79bf26fb6176fc0050370725e48
Philip Indra Prayitno <philip.prayitno@binus.edu>
2021-04-14 21:44:52 +0700

    feat: header responsive

--- header.js
+++ header.js
@@ -1,12 +1,38 @@
-document.addEventListener('DOMContentLoaded', function() {
-    window.addEventListener('scroll', sticky);
-    var navbar = document.getElementById("header-navbar");
-    var sticky = navbar.offsetTop
+// document.addEventListener('DOMContentLoaded', function() {
+//     window.addEventListener('scroll', sticky);
+//     var navbar = document.getElementById("header-navbar");
+//     var sticky = navbar.offsetTop
 
-    function sticky() {
-      navbar.classList.toggle("sticky", window.scrollY > 0)
+//     function sticky() {
+//       navbar.classList.toggle("sticky", window.scrollY > 0)
+//     }
+// })
+
+var isToggled = false;
+
+function toggleMenu() {
+  var navbar = document.getElementById("header-navbar-menu");
+  if(navbar.style.display == 'none') {
+    navbar.style.display = 'block';
+    isToggled = true;
+  } else {
+    navbar.style.display = 'none';
+    isToggled = false;
+  }
+}
+
+function configureHeader() {
+  
+  var navbar = document.getElementById("header-navbar-menu");
+  if($(document).width() >= 800) {
+    navbar.style.display = 'flex';
+    isToggled = false;
+  }else{
+    if(!isToggled) {
+      navbar.style.display = 'none';
     }
-})
+  }
+}
 
 //https://www.freepik.com/free-vector/player-characters-online-game-concept_8131809.htm#page=1&query=gamer&position=32
 //https://www.flaticon.com/free-icon/protection-shield-with-a-check-mark_315#
